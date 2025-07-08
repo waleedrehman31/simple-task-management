7bdcf82a2cd5c81e81547e2fcaa02d47fcac88ef
waleedrehman31 <waleedrehman31@gmail.com>
2025-07-08 10:48:25 +0500

    chore: setting up fronend layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <x-layouts.partials.navbar />
+    {{ $slot }}
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+
+</html>
